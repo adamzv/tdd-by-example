5cee2985311d3f10813730f3188c35a7313fe71e
Adam Zverka <adamzverka@hotmail.com>
2022-10-29 20:17:59 +0200

    Implementation of Money classes and test

---
+++ src/main/java/guru/springframework/Money.java
@@ -0,0 +1,31 @@
+package guru.springframework;
+
+public abstract class Money {
+    protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    protected String currency() {
+        return currency;
+    };
+
+    public abstract Money times(int multiplier);
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount, "CHF");
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Money money = (Money) obj;
+        return amount == money.amount && getClass().equals(obj.getClass());
+    }
+}
